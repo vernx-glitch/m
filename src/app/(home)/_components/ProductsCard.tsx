cc54eab6efb065631c0a916181887b911d6d50ed
vernx-glitch <kamba12samuel@gmail.com>
2025-08-16 01:52:58 +0530

    Update ProductsCard.tsx

--- src/app/(home)/_components/ProductsCard.tsx
+++ src/app/(home)/_components/ProductsCard.tsx
@@ -20,7 +20,10 @@ interface ProductsCardProps {
 
 export default function ProductsCard({ d }: ProductsCardProps) {
   return (
-    <Link href={`https://www.vygaming.id/id/${d.handle}`} className="hover:scale-105 transition-all duration-300 ease-out">
+    <Link
+      href={`https://www.vernx.shop/id/${d.handle}`}
+      className="hover:scale-105 transition-all duration-300 ease-out"
+    >
       <div
         className={`animate-fade-up animate-duration-300 group relative h-full w-full flex flex-col bg-[#212121] rounded-2xl overflow-hidden hover:ring-2 hover:ring-offset-2 hover:ring-offset-[#141414] hover:ring-[#0563FC] transition-all duration-300 ease-in-out`}
       >
